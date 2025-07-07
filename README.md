da9cbd570276050b0895b079d365cb99a2b8a5b3
JoaoGeraldo <joaogeraldo40@gmail.com>
2025-07-07 22:31:54 +0200

    Core: Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GastroRoots
+GastroRoots is a RESTful API for managing restaurants, orders, users, payments and more - Built with Spring Boot, Spring Data JPA and Spring Security, following best practices for cloud-native development.
---
+++ src/main/java/com/gastro/roots/GastroRootsApiApplication.java
@@ -0,0 +1,13 @@
+package com.gastro.roots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastroRootsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastroRootsApiApplication.class, args);
+	}
+
+}
